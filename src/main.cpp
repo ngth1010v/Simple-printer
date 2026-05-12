11c294c3949182656a89ed82c6675e6e706351a6
ngth1010v <thinh0947709008@gmail.com>
2026-05-12 15:12:43 +0700

    Refactor: convert path in main/app to wstring

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,5 @@
-// #include <windows.h>
-// #include <commctrl.h>
-// #include "app/CommandLine.h"
-// #include "app/App.h"
-
-// int main(int argc, char** argv) {
-//     INITCOMMONCONTROLSEX icc = {};
-//     icc.dwSize = sizeof(icc);
-//     icc.dwICC = ICC_WIN95_CLASSES;
-//     InitCommonControlsEx(&icc);
-
-//     // parse CLI
-//     CommandLine::Parse(argc, argv);
-
-//     // run app
-//     return App::Run();
-// }
-
+#include "app/CommandLine.h"
+#include "app/App.h"
 
 #include <windows.h>
 #include <commctrl.h>
