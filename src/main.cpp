448567c0000618fa7c3e577a290d4c287a4227c2
ngth1010v <thinh0947709008@gmail.com>
2026-05-12 14:45:32 +0700

    Refactor: convert main to wWinMain

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,43 @@
+// #include <windows.h>
+// #include <commctrl.h>
+// #include "app/CommandLine.h"
+// #include "app/App.h"
+
+// int main(int argc, char** argv) {
+//     INITCOMMONCONTROLSEX icc = {};
+//     icc.dwSize = sizeof(icc);
+//     icc.dwICC = ICC_WIN95_CLASSES;
+//     InitCommonControlsEx(&icc);
+
+//     // parse CLI
+//     CommandLine::Parse(argc, argv);
+
+//     // run app
+//     return App::Run();
+// }
+
+
 #include <windows.h>
 #include <commctrl.h>
-#include "app/CommandLine.h"
-#include "app/App.h"
 
-int main(int argc, char** argv) {
-    INITCOMMONCONTROLSEX icc = {};
-    icc.dwSize = sizeof(icc);
-    icc.dwICC = ICC_WIN95_CLASSES;
-    InitCommonControlsEx(&icc);
+#include <string>
+#include <sstream>
 
-    // parse CLI
+
+int WINAPI wWinMain(HINSTANCE, HINSTANCE, PWSTR, int)
+{
+    int argc = 0;
+    LPWSTR* argv = CommandLineToArgvW(GetCommandLineW(), &argc);
+
+    if (!argv) {
+        return 1;
+    }
+
     CommandLine::Parse(argc, argv);
 
-    // run app
-    return App::Run();
+    int result = App::Run();
+
+    LocalFree(argv);
+
+    return result;
 }
